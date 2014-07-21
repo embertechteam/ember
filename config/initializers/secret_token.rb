ac78a166875b9c99e065e9586189460ce1e5e2e0
embertechteam <ember.techteam@gmail.com>
2014-07-21 15:10:09 +0800

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Website::Application.config.secret_key_base = '68d095430469e2dc233c9f6b2620f47206cea430bef2ea3b06dbcabcfd386f3b3c15679efc29d0d093eb98ebcb8ea134db15efd4121b6475260f89678c097bae'
