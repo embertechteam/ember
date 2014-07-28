d9aee543762778d56960a1784616eb6e9eef9ca7
Joseph Chiang <josephchiang28@gmail.com>
2014-07-28 21:05:16 +0800

    Modified omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,9 +1,6 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '679060465515672', 'c52876d8d5d2e4356ba6bb21e0be2044', {:client_options => {:ssl => {:ca_file => Rails.root.join("cacert.pem").to_s}}}
-end
-
-Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :facebook, '679060465515672', 'c52876d8d5d2e4356ba6bb21e0be2044', {:client_options => {:ssl => {:ca_file => Rails.root.join("cacert.pem").to_s}}, :scope => 'email', :display => 'popup'} 
   provider :google_oauth2, '213310765497-r7a6ikm6f6u0orcfdqh964vmqchqalgn.apps.googleusercontent.com', 'dmmxt5JsOo7XbGvHGy-9ofMa', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
 end
