7e84a2180ef4dc7b6ee4388ec9db58480cc15fc2
Joseph Chiang <josephchiang28@gmail.com>
2014-07-28 21:14:10 +0800

    Removed not working popup code

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '679060465515672', 'c52876d8d5d2e4356ba6bb21e0be2044', {:client_options => {:ssl => {:ca_file => Rails.root.join("cacert.pem").to_s}}, :scope => 'email', :display => 'popup'} 
+  provider :facebook, '679060465515672', 'c52876d8d5d2e4356ba6bb21e0be2044', {:client_options => {:ssl => {:ca_file => Rails.root.join("cacert.pem").to_s}}, :scope => 'email'} 
   provider :google_oauth2, '213310765497-r7a6ikm6f6u0orcfdqh964vmqchqalgn.apps.googleusercontent.com', 'dmmxt5JsOo7XbGvHGy-9ofMa', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
 end
